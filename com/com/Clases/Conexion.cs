0e2a8c48ecf300b41e44eaa166660f7e47403190
JENNIFER ALEJANDRA GARCIA HERNANDEZ <2016GH208@unicaescri.onmicrosoft.com>
2018-10-02 22:14:51 -0600

    Conexion

--- com/com/Clases/Conexion.cs
+++ com/com/Clases/Conexion.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
 
-public class Class1
+
+namespace SisCoAcssUni_Model.clases
 {
-	public Class1()
-	{
-	}
+    public class conexion
+    {
+        public MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conectar = new MySqlConnection("server=localhost; database=control_alumnos; Uid=root; pwd=Arnold;");
+            conectar.Open();
+            return conectar;
+        }
+    }
 }
+	
